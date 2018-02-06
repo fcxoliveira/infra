c0a737f99a60b5d32cbd3e66759aa2c2f9b5767d
fcxoliveira <filcarx@gmail.com>
2018-02-06 21:13:54 -0200

    Eventos criados

--- fsm_config.h
+++ fsm_config.h
@@ -2,16 +2,15 @@
 #define NUM_EVENTS 5
 
 // definicao dos possiveis eventos gerados pelos estados da FSM
-typedef enum event_ {int_connect, server, question, send_button, send_time} event;
+typedef enum event_ {internet_c, server, question, send_button, send_time} event;
 // definicao das funcoes que implementam o comportamento de cada estado
 event int_connect_state(void);
 event mqqt_state(void);
-event time_pass_state(void);
-event btn_pressed_state(void);
-event send_data_state(void);
-event back_state(void);
+event question_state(void);
+event send_button_state(void);
+event send_time_state(void);
 // array de ponteiros para as funcoes dos estados
-event (* state_functions[])(void) = {int_connect_state, time_pass_state, btn_pressed_state, send_data_state, back_state };
+event (* state_functions[])(void) = {int_connect_state, time_pass_state, send_button_state, send_time_state };
 // definicao dos nomes dos estados
 typedef enum state_ {internet, mqqt, question, send_button, send_time} state;
 // estrutura que define as transicoes dos estados
@@ -28,4 +27,3 @@ state state_transitions[NUM_STATES][NUM_EVENTS] = {{internet, mqqt, internet, in
 state lookup_transitions(state cur_state, event cur_evt) {
   return state_transitions[cur_state][cur_evt];
 }
-
--- program_ino.ino
+++ program_ino.ino
@@ -1,56 +1,109 @@
 #include "fsm_config.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <DHT.h>;
 
+#define DHTPIN D3          // what pin we're connected to
+#define DHTTYPE DHT22     // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 const int buttonPin = D5;    // definicao do pino utilizado pelo botao
 const int ledPin = D7;       // definicao do pino utilizado pelo led
-const char* ssid = "jualabs"; // id da rede
+const char* ssid = "GVT-AE52"; // id da rede
 const char* password = "jualabsufrpe"; // senha da rede
 const char* mqtt_server = "things.ubidots.com"; // server
+
 
 int buttonState = LOW;             // armazena a leitura atual do botao
 int lastButtonState = LOW;         // armazena a leitura anterior do botao
 unsigned long lastDebounceTime = 0;  // armazena a ultima vez que a leitura da entrada variou
 unsigned long debounceDelay = 50;    // tempo utilizado para implementar o debounce
+float umidade;  //Stores humidity value
+float temperatura; //Stores temperature value
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
-char msg[50];
-int value = 0;
-int i = 0;
+char msg[60];
+char msg1[60];
+
 
 
 // definicao das funcoes relativas a cada estado
-
-event btn_pressed_state(void) {
-  digitalWrite(ledPin, HIGH);
-  if(read_button())
-
-    return btn_pressed;
-}
-event mqqt(void){
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  return time_pass;
-}
+event mqqt_state(void){
+    if (client.connect("ESP8266Client","A1E-KjZv9M3SE1Zkom6cZM1HS3bcUabMLF","")) {
+      Serial.println("connected");
+      client.subscribe("inTopic");
+      return question;
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+      return server;
+    }
+  }
 
 event question_state(void){
   now = millis();
   if (readButton()){
     digitalWrite(led, HIGH);
-    send_data();
     return send_button;
   }
-  else if(now-millis() == 10000){
-    now = millis()
-    sendData();
+
+  else if(now- lastMsg >= 10000){
+    digitalWrite(led, HIGH);
+    lastMsg = now
     return send_time;
   }
   else{
-    return question_state;
+    digitalWrite(led, LOW);
+    return question;
   }
+}
+
+event send_button_state(void) {
+  if (send_data()){
+    return question;
+  }
+  else{
+    return send_button;
+  }
+}
+
+event send_time_state(void) {
+  // Loop until we're reconnected
+  if (send_data()){
+    return question;
+  }
+  else{
+    return send_time;
+  }
+}
+
+event int_connect_state(void) {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  if (WiFi.status()== WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    return server;
+  }
+  else{
+    Serial.println("Wifi não conectada")
+    return internet_c;
+  }
+
+
 }
 
 
@@ -79,102 +132,35 @@ int read_button() {
   }
   return false;
 }
+int send_data(void) {
+  umidade = dht.readHumidity();
+  temperatura = dht.readTemperature();
+  snprintf (msg, 60, "{\"value\":%d}", dtostrf(temperatura, 6, 2, NULL));
+  snprintf (msg1, 60, "{\"value\":%d}", dtostrf(umidade, 6, 2, NULL));
+  client.publish("/v1.6/devices/wemos/umidade", msg1);
+  client.publish("/v1.6/devices/wemos/temp", msg);
+  //Print temp and humidity values to serial monitor
+  Serial.print("Humidity: ");
+  Serial.print(umidade);
+  Serial.print(" %, Temp: ");
+  Serial.print(temperatura);
+  Serial.println(" Celsius");
+  delay(10000); //Delay 2 sec.
+  return true
+}
 
 void setup() {
+  client.setServer(mqtt_server, 1883);
+  dht.begin();
   pinMode(buttonPin, INPUT);
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
 }
 
-void int_connect_state() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  if (WiFi.status()== WL_CONNECTED) {
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    return mqqt;
-  }
-  else{
-    Serial.println("Wifi não conectada")
-    return int_connect;
-  }
-
-
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
-}
-
-void send_data() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266Client","A1E-KjZv9M3SE1Zkom6cZM1HS3bcUabMLF","")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("/v1.6/devices/wemos/contador", "{\"value\":3}");
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-/*void loop() {
+void loop() {
   cur_state_function = state_functions[cur_state];
   cur_evt = (event) cur_state_function();
   if (EXIT_STATE == cur_state)
     return;
   cur_state = lookup_transitions(cur_state, cur_evt);
-}
-*/
-void loop () {
-
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, 75, "hello world #%ld", value);
-    if (i==0){
-    i  = i + 1;
-    client.publish("/v1.6/devices/wemos/contador", "{\"value\":100}");
-  }
-  }
 }
